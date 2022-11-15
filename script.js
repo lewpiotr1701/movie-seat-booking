ccc868443d05d95e9fa7f7169e3b74b650baf2b6
Piotr Lewczyk <lewczykpiotr@gmail.com>
2022-11-15 21:55:10 +0100

    update count and total proce info

--- script.js
+++ script.js
@@ -6,15 +6,28 @@ const movieSelect = document.querySelector("#movie");
 
 const ticketPrice = parseInt(movieSelect.value, 10);
 
-// Add event listener to hallContainer because
-// this way it will not be neccessary to add event listener to each seat
-// but only to the seat that was clicked on
-hallContainer.addEventListener("click", selectSeat)
-
 function selectSeat(e) {
 
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
         e.target.classList.toggle("selected");
+
+        updateSelectedInfo();
+
     }
+}
+
+// Update count and total
+function updateSelectedInfo() {
+
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    count.textContent = selectedSeats.length;
+
+    total.textContent = selectedSeats.length * ticketPrice;
 
 }
+
+// Add event listener to hallContainer because
+// this way it will not be neccessary to add event listener to each seat
+// but only to the seat that was clicked on
+hallContainer.addEventListener("click", selectSeat);
