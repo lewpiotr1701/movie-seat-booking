1512dfe93b85c798b3a4670e250e14820025f751
Piotr Lewczyk <lewczykpiotr@gmail.com>
2022-11-16 10:03:20 +0100

    populate UI when reloading page

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const hallContainer = document.querySelector(".hall-container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied");
+const seatsAll = document.querySelectorAll(".row .seat");
+const seatsAvailable = document.querySelectorAll(".row .seat:not(.occupied");
 const count = document.querySelector("#count");
 const total = document.querySelector("#total");
 const movieSelect = document.querySelector("#movie");
 
+populateUI();
+
+updateSelectedInfo();
+
 let ticketPrice = parseInt(movieSelect.value, 10);
 
+// Mark selected seat with .selected class
 function selectSeat(e) {
 
     if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
@@ -21,25 +27,47 @@ function updateSelectedInfo() {
 
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-    const allSeats = document.querySelectorAll(".row .seat");
-
     const selectedSeatsIndex = [...selectedSeats].map(function (seat) {
-        return [...allSeats].indexOf(seat);
+        return [...seatsAll].indexOf(seat);
     });
 
     localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndex))
 
     count.innerText = selectedSeats.length;
 
+    const ticketPrice = parseInt(movieSelect.value, 10);
+
     total.innerText = selectedSeats.length * ticketPrice;
 
 }
 
 // Save selected movie index and price into Local Storage
-function setMovieData(index, price) {
+function setMovieData(movieIndex, moviePrice) {
 
-    localStorage.setItem("selectedMovieIndex", index);
-    localStorage.setItem("selectedMoviePrice", price);
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice);
+
+}
+
+// Get data from Local Storage and populate UI
+function populateUI() {
+
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+    // Check if something is in Local Storage && and if it is check if it is not an empty array
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+
+        selectedSeats.forEach((seatIndex) => {
+            [...seatsAll][seatIndex].classList.add("selected");
+        });
+
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 
 }
 
@@ -50,7 +78,6 @@ hallContainer.addEventListener("click", selectSeat);
 
 // Movie select event
 movieSelect.addEventListener("change", e => {
-    ticketPrice = parseInt(movieSelect.value, 10);
     setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedInfo();
 });
